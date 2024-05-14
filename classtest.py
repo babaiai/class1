b64c3687e6d2c06b5c408dc82a1f983f9b38a990
babaiai <168524852+babaiai@users.noreply.github.com>
2024-05-14 14:07:41 +0800

    Update classtest.py

--- classtest.py
+++ classtest.py
@@ -32,7 +32,12 @@ stc.html(html_temp)
 # df_original.to_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
 
 ## 读取Pickle文件
-df_original = pd.read_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
+@st.cache_data(ttl=3600,show_spinner="正在加載資料...")
+def load_data(url):
+	df=pd.read_pickle(url)
+	reyurn df
+df_original = load_data('kbars_2330_2022-01-01-2022-11-18.pkl')
+#df_original = pd.read_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
 
 
 #df.columns  ## Index(['Unnamed: 0', 'time', 'open', 'low', 'high', 'close', 'volume','amount'], dtype='object')
