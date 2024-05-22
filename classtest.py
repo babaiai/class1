c9f67a9262983c7caac27c6f916bd27673f50579
babaiai <168524852+babaiai@users.noreply.github.com>
2024-05-22 15:37:20 +0800

    Update classtest.py

--- classtest.py
+++ classtest.py
@@ -36,8 +36,8 @@ df_original.to_pickle('kbars_6560.pkl')
 def load_data(url):
 	df=pd.read_pickle(url)
 	return df
-df_original = load_data('kbars_2330_2022-01-01-2022-11-18.pkl')
-df_original = pd.read_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
+df_original = load_data('kbars_6560.pkl')
+df_original = pd.read_pickle('kbars_6560.pkl')
 
 
 #df.columns  ## Index(['Unnamed: 0', 'time', 'open', 'low', 'high', 'close', 'volume','amount'], dtype='object')
