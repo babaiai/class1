6f3d89c2fd49e34c25d87b69d5ef86f042a78c29
babaiai <168524852+babaiai@users.noreply.github.com>
2024-05-22 15:51:11 +0800

    Update classtest.py

--- classtest.py
+++ classtest.py
@@ -55,14 +55,14 @@ print(df_original.columns)
 
 
 ##### 選擇資料區間
-st.subheader("選擇開始與結束的日期, 區間:2019-01-02 至 2024-05-21")
-start_date = st.text_input('選擇開始日期 (日期格式: 2019-01-02)',datetime.date(2019, 1, 2), min_value=datetime.date(2019, 1, 2), max_value=datetime.date(2024, 5, 21))
-end_date = st.text_input('選擇結束日期 (日期格式: 2024-05-21)',  datetime.date(2024-05-21), datetime.date(2024, 5, 21), min_value=datetime.date(2019, 1, 2), max_value=datetime.date(2024, 5, 21))
-start_date = datetime.datetime.combine(start_date, datetime.datetime.min.time())
-end_date = datetime.datetime.combine(end_date, datetime.datetime.min.time())
+start_date = st.text_input('選擇開始日期 (日期格式: 2022-01-03)', '2022-01-03')
+end_date = st.text_input('選擇結束日期 (日期格式: 2022-11-18)', '2022-11-18')
+start_date = datetime.datetime.strptime(start_date,'%Y-%m-%d')
+end_date = datetime.datetime.strptime(end_date,'%Y-%m-%d')
+
+
 # 使用条件筛选选择时间区间的数据
-mask = (df_original['time'] >= start_date) & (df_original['time'] <= end_date)
-df = df_original[mask]
+df = df_original[(df_original['time'] >= start_date) & (df_original['time'] <= end_date)]
 
 
 ###### (2) 轉化為字典 ######:
