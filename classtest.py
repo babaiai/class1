f8703625ff27144c7ae0142c8a94731e7d9db2c0
babaiai <168524852+babaiai@users.noreply.github.com>
2024-05-22 15:05:06 +0800

    Update classtest.py

--- classtest.py
+++ classtest.py
@@ -26,17 +26,17 @@ stc.html(html_temp)
 #df = pd.read_excel("kbars_2330_2022-07-01-2022-07-31.xlsx")
 
 # ## 讀取 excel 檔
-# df_original = pd.read_excel("kbars_2330_2022-01-01-2022-11-18.xlsx")
+df_original = pd.read_excel("kbars_2330_2022-01-01-2022-11-18.xlsx")
 
 # ## 保存为Pickle文件:
 # df_original.to_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
 
 ## 读取Pickle文件
-@st.cache_data(ttl=3600,show_spinner="正在加載資料...")
-def load_data(url):
-	df=pd.read_pickle(url)
-	return df
-df_original = load_data('kbars_2330_2022-01-01-2022-11-18.pkl')
+#@st.cache_data(ttl=3600,show_spinner="正在加載資料...")
+#def load_data(url):
+	#df=pd.read_pickle(url)
+	#return df
+#df_original = load_data('kbars_2330_2022-01-01-2022-11-18.pkl')
 #df_original = pd.read_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
 
 
