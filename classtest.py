35bf77b55be6f64a040407dfc28d61c917f3b138
babaiai <168524852+babaiai@users.noreply.github.com>
2024-05-28 15:28:11 +0800

    Update classtest.py

--- classtest.py
+++ classtest.py
@@ -269,16 +269,23 @@ with st.expander("K線圖, 移動平均線"):
     
     #### include a go.Bar trace for volumes
     # 确保 last_nan_index_MA 不是 None，且 'Time' 列和 'MA_long'、'MA_short' 列都包含有效的数据
-if last_nan_index_MA is not None and 'Time' in KBar_df.columns and 'MA_long' in KBar_df.columns and 'MA_short' in KBar_df.columns:
-    fig1.add_trace(go.Bar(x=KBar_df['Time'], y=KBar_df['Volume'], name='成交量', marker=dict(color='black')), secondary_y=False)
-    fig1.add_trace(go.Scatter(x=KBar_df['Time'][last_nan_index_MA+1:], y=KBar_df['MA_long'][last_nan_index_MA+1:], mode='lines', line=dict(color='orange', width=2), name=f'{LongMAPeriod}-根 K棒 移動平均線'), 
-                    secondary_y=True)
-    fig1.add_trace(go.Scatter(x=KBar_df['Time'][last_nan_index_MA+1:], y=KBar_df['MA_short'][last_nan_index_MA+1:], mode='lines', line=dict(color='pink', width=2), name=f'{ShortMAPeriod}-根 K棒 移動平均線'), 
-                    secondary_y=True)
-    fig1.layout.yaxis2.showgrid = True
-    st.plotly_chart(fig1, use_container_width=True)
+# 确保 last_nan_index_RSI 不是 None，且 'Time' 列和 'RSI_long'、'RSI_short' 列都包含有效的数据
+if last_nan_index_RSI is not None and 'Time' in KBar_df.columns and 'RSI_long' in KBar_df.columns and 'RSI_short' in KBar_df.columns:
+    fig2.add_trace(go.Candlestick(x=KBar_df['Time'],
+                    open=KBar_df['Open'], high=KBar_df['High'],
+                    low=KBar_df['Low'], close=KBar_df['Close'], name='K線'),
+                   secondary_y=True)   ## secondary_y=True 表示此圖形的y軸scale是在右邊而不是在左邊
+    
+    fig2.add_trace(go.Scatter(x=KBar_df['Time'][last_nan_index_RSI+1:], y=KBar_df['RSI_long'][last_nan_index_RSI+1:], mode='lines',line=dict(color='red', width=2), name=f'{LongRSIPeriod}-根 K棒 移動 RSI'), 
+                    secondary_y=False)
+    fig2.add_trace(go.Scatter(x=KBar_df['Time'][last_nan_index_RSI+1:], y=KBar_df['RSI_short'][last_nan_index_RSI+1:], mode='lines',line=dict(color='blue', width=2), name=f'{ShortRSIPeriod}-根 K棒 移動 RSI'), 
+                    secondary_y=False)
+
+    fig2.layout.yaxis2.showgrid=True
+    st.plotly_chart(fig2, use_container_width=True)
 else:
     st.error("出现错误：无法绘制图形，请检查数据是否有效。")
+
 
 
 
